cb3fbc7519877c0385238abd0f8d8c1606f25b8c
definetly-not-carl <cmoroni04@gmail.com>
2023-01-10 19:43:06 +0100

    first file, but it doesn't do much

---
+++ encrypt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	cout << "Hi there, this is an encrypting program. Enter the sentence that you want encrypt";
+	string sentence;
+
+	cin>>sentence;
+
+	return 0;
+}
