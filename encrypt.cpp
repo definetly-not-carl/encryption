465920ef1af0434d868bfe49bb6950768035ebe8
definetly-not-carl <cmoroni04@gmail.com>
2023-01-11 15:29:42 +0100

    beginned the creation of the encrypting function

--- encrypt.cpp
+++ encrypt.cpp
@@ -3,11 +3,18 @@
 
 using namespace std;
 
+string encrypt (string input){
+	//idk, it should do something
+	return input;
+}
+
 int main(){
 	cout << "Hi there, this is an encrypting program. Enter the sentence that you want encrypt";
 	string sentence;
 
 	cin>>sentence;
+
+	string encrypted = encrypt(sentence);
 
 	return 0;
 }
